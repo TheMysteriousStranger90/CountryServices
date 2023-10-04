cb66bea54c2bc0ffabd43bb07be08b7fa2abc838
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-10-04 11:22:21 +0300

    ver. 2 (still doesn't work full...)

--- CountryServices/CountryService.cs
+++ CountryServices/CountryService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -26,9 +27,7 @@ namespace CountryServices
             this.serviceUrl = serviceUrl;
             this.httpClient = new HttpClient();
         }
-
-        //
-
+        
         /// <summary>
         /// Gets information about currency by country code synchronously.
         /// </summary>
@@ -38,27 +37,72 @@ namespace CountryServices
         /// </see>
         /// <returns>Information about country currency as <see cref="LocalCurrency"/>>.</returns>
         /// <exception cref="ArgumentException">Throw if countryCode is null, empty, whitespace or invalid country code.</exception>
-        public LocalCurrency GetLocalCurrencyByAlpha2Or3Code(string? alpha2Or3Code)
+public LocalCurrency GetLocalCurrencyByAlpha2Or3Code(string? alpha2Or3Code)
+{
+    if (string.IsNullOrWhiteSpace(alpha2Or3Code) || alpha2Or3Code == "UPSS")
+    {
+        throw new ArgumentException("Invalid country code.");
+    }
+
+    if (currencyCache.TryGetValue(alpha2Or3Code, out var weakReference) && weakReference.TryGetTarget(out var cachedCurrency))
+    {
+        return cachedCurrency;
+    }
+
+    try
+    {
+        string apiUrl = $"{serviceUrl}/alpha/{Uri.EscapeDataString(alpha2Or3Code)}";
+        string json = httpClient.GetStringAsync(apiUrl).Result;
+
+        // Parse the JSON response into an object.
+        var jsonObject = JsonDocument.Parse(json).RootElement;
+
+        string name = jsonObject.GetProperty("name").GetString();
+
+        // Access the currencies property as an object.
+        var currencies = jsonObject.GetProperty("currencies");
+
+        string currencyCode = currencies.EnumerateObject()
+            .SelectMany(prop => prop.Value.EnumerateObject())
+            .Where(prop => prop.Name.Equals("code"))
+            .Select(prop => prop.Value.GetString())
+            .FirstOrDefault();
+
+        string currencySymbol = currencies.EnumerateObject()
+            .SelectMany(prop => prop.Value.EnumerateObject())
+            .Where(prop => prop.Name.Equals("symbol"))
+            .Select(prop => prop.Value.GetString())
+            .FirstOrDefault();
+
+        LocalCurrency localCurrency = new LocalCurrency
         {
-            if (string.IsNullOrWhiteSpace(alpha2Or3Code) || alpha2Or3Code == "UPSS")
-            {
-                throw new ArgumentException("Invalid country code.");
-            }
-            
-            if (currencyCache.TryGetValue(alpha2Or3Code, out var weakReference) && weakReference.TryGetTarget(out var cachedCurrency))
-            {
-                return cachedCurrency;
-            }
+            CountryName = name,
+            CurrencyCode = currencyCode,
+            CurrencySymbol = currencySymbol
+        };
 
-            string apiUrl = $"{serviceUrl}/alpha/{alpha2Or3Code}";
-            string json = httpClient.GetStringAsync(apiUrl).Result;
-            
-            LocalCurrency currency = JsonSerializer.Deserialize<LocalCurrency>(json);
-            
-            currencyCache[alpha2Or3Code] = new WeakReference<LocalCurrency>(currency);
+        // Cache the currency
+        currencyCache[alpha2Or3Code] = new WeakReference<LocalCurrency>(localCurrency);
 
-            return currency;
-        }
+        return localCurrency;
+    }
+    catch (HttpRequestException ex)
+    {
+        throw new ApplicationException("Failed to retrieve country info. Check your network connection.", ex);
+    }
+    catch (JsonException ex)
+    {
+        throw new JsonException("Failed to parse JSON response.", ex);
+    }
+    catch (Exception ex)
+    {
+        throw new ApplicationException("An error occurred while retrieving country info.", ex);
+    }
+}
+
+
+
+
 
         /// <summary>
         /// Gets information about currency by country code asynchronously.
@@ -95,15 +139,54 @@ namespace CountryServices
         {
             if (string.IsNullOrWhiteSpace(capital) || capital == "UPSS")
             {
-                throw new ArgumentException("Capital name is invalid.");
+                throw new ArgumentException("Invalid capital name.");
             }
 
-            string apiUrl = $"{serviceUrl}/capital/{capital}";
-            string json = httpClient.GetStringAsync(apiUrl).Result;
-            
-            CountryInfo countryInfo = JsonSerializer.Deserialize<CountryInfo>(json);
+            try
+            {
+                string apiUrl = $"{serviceUrl}/capital/{Uri.EscapeDataString(capital)}";
+                string json = httpClient.GetStringAsync(apiUrl).Result;
 
-            return countryInfo;
+                // Parse the JSON response into an array of JSON elements.
+                JsonDocument doc = JsonDocument.Parse(json);
+
+                // Assuming the JSON response is an array, loop through the elements.
+                foreach (var element in doc.RootElement.EnumerateArray())
+                {
+                    // Extract the properties you need from each element.
+                    string name = element.GetProperty("name").GetString();
+                    string capitalName = element.GetProperty("capital").GetString();
+                    double area = element.GetProperty("area").GetDouble();
+                    int population = element.GetProperty("population").GetInt32();
+                    string flag = element.GetProperty("flag").GetString();
+
+                    // Create and return a CountryInfo object.
+                    CountryInfo countryInfo = new CountryInfo
+                    {
+                        Name = name,
+                        CapitalName = capitalName,
+                        Area = area,
+                        Population = population,
+                        Flag = flag
+                    };
+
+                    return countryInfo;
+                }
+
+                throw new JsonException("No valid country information found in the JSON array.");
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new ApplicationException("Failed to retrieve country info. Check your network connection.", ex);
+            }
+            catch (JsonException ex)
+            {
+                throw new JsonException("Failed to parse JSON response.", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("An error occurred while retrieving country info.", ex);
+            }
         }
 
         /// <summary>
@@ -117,15 +200,53 @@ namespace CountryServices
         {
             if (string.IsNullOrWhiteSpace(capital) || capital == "UPSS")
             {
-                throw new ArgumentException("Capital name is invalid.");
+                throw new ArgumentException("Invalid capital name.");
             }
 
-            string apiUrl = $"{serviceUrl}/capital/{capital}";
-            string json = await httpClient.GetStringAsync(apiUrl, token);
-            
-            CountryInfo countryInfo = JsonSerializer.Deserialize<CountryInfo>(json);
+            try
+            {
+                string apiUrl = $"{serviceUrl}/capital/{Uri.EscapeDataString(capital)}";
+                string json = await httpClient.GetStringAsync(apiUrl);
 
-            return countryInfo;
+                // Parse the JSON response into an array of JSON elements.
+                JsonDocument doc = JsonDocument.Parse(json);
+
+                foreach (var element in doc.RootElement.EnumerateArray())
+                {
+                    // Extract the properties you need from each element.
+                    string name = element.GetProperty("name").GetString();
+                    string capitalName = element.GetProperty("capital").GetString();
+                    double area = element.GetProperty("area").GetDouble();
+                    int population = element.GetProperty("population").GetInt32();
+                    string flag = element.GetProperty("flag").GetString();
+
+                    CountryInfo countryInfo = new CountryInfo
+                    {
+                        Name = name,
+                        CapitalName = capitalName,
+                        Area = area,
+                        Population = population,
+                        Flag = flag
+                    };
+
+                    return countryInfo;
+                }
+
+                throw new JsonException("No valid country information found in the JSON array.");
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new ApplicationException("Failed to retrieve country info. Check your network connection.", ex);
+            }
+            catch (JsonException ex)
+            {
+                throw new JsonException("Failed to parse JSON response.", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("An error occurred while retrieving country info.", ex);
+            }
         }
+
     }
 }
