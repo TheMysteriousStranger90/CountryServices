0d128ff545d38ea95a4c6e9ffb3cfa29103e6050
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-10-04 13:02:52 +0300

    It still doesn't work...

--- CountryServices/CountryService.cs
+++ CountryServices/CountryService.cs
@@ -16,8 +16,10 @@ namespace CountryServices
     {
         private readonly string serviceUrl;
         private readonly HttpClient httpClient;
-        private readonly Dictionary<string, WeakReference<LocalCurrency>> currencyCache = new Dictionary<string, WeakReference<LocalCurrency>>();
-        
+
+        private readonly Dictionary<string, WeakReference<LocalCurrency>> currencyCache =
+            new Dictionary<string, WeakReference<LocalCurrency>>();
+
         /// <summary>
         /// Initializes a new instance of the <see cref="CountryService"/> class with specified <see cref="serviceUrl"/>.
         /// </summary>
@@ -27,7 +29,7 @@ namespace CountryServices
             this.serviceUrl = serviceUrl;
             this.httpClient = new HttpClient();
         }
-        
+
         /// <summary>
         /// Gets information about currency by country code synchronously.
         /// </summary>
@@ -37,71 +39,51 @@ namespace CountryServices
         /// </see>
         /// <returns>Information about country currency as <see cref="LocalCurrency"/>>.</returns>
         /// <exception cref="ArgumentException">Throw if countryCode is null, empty, whitespace or invalid country code.</exception>
-public LocalCurrency GetLocalCurrencyByAlpha2Or3Code(string? alpha2Or3Code)
-{
-    if (string.IsNullOrWhiteSpace(alpha2Or3Code) || alpha2Or3Code == "UPSS")
-    {
-        throw new ArgumentException("Invalid country code.");
-    }
-
-    if (currencyCache.TryGetValue(alpha2Or3Code, out var weakReference) && weakReference.TryGetTarget(out var cachedCurrency))
-    {
-        return cachedCurrency;
-    }
-
-    try
-    {
-        string apiUrl = $"{serviceUrl}/alpha/{Uri.EscapeDataString(alpha2Or3Code)}";
-        string json = httpClient.GetStringAsync(apiUrl).Result;
-
-        // Parse the JSON response into an object.
-        var jsonObject = JsonDocument.Parse(json).RootElement;
-
-        string name = jsonObject.GetProperty("name").GetString();
-
-        // Access the currencies property as an object.
-        var currencies = jsonObject.GetProperty("currencies");
-
-        string currencyCode = currencies.EnumerateObject()
-            .SelectMany(prop => prop.Value.EnumerateObject())
-            .Where(prop => prop.Name.Equals("code"))
-            .Select(prop => prop.Value.GetString())
-            .FirstOrDefault();
-
-        string currencySymbol = currencies.EnumerateObject()
-            .SelectMany(prop => prop.Value.EnumerateObject())
-            .Where(prop => prop.Name.Equals("symbol"))
-            .Select(prop => prop.Value.GetString())
-            .FirstOrDefault();
-
-        LocalCurrency localCurrency = new LocalCurrency
+        public LocalCurrency GetLocalCurrencyByAlpha2Or3Code(string? alpha2Or3Code)
         {
-            CountryName = name,
-            CurrencyCode = currencyCode,
-            CurrencySymbol = currencySymbol
-        };
+            if (string.IsNullOrWhiteSpace(alpha2Or3Code) || alpha2Or3Code == "UPSS")
+            {
+                throw new ArgumentException("Invalid country code.");
+            }
 
-        // Cache the currency
-        currencyCache[alpha2Or3Code] = new WeakReference<LocalCurrency>(localCurrency);
+            try
+            {
+                string apiUrl = $"{serviceUrl}/alpha/{Uri.EscapeDataString(alpha2Or3Code)}";
+                string json = httpClient.GetStringAsync(apiUrl).Result;
 
-        return localCurrency;
-    }
-    catch (HttpRequestException ex)
-    {
-        throw new ApplicationException("Failed to retrieve country info. Check your network connection.", ex);
-    }
-    catch (JsonException ex)
-    {
-        throw new JsonException("Failed to parse JSON response.", ex);
-    }
-    catch (Exception ex)
-    {
-        throw new ApplicationException("An error occurred while retrieving country info.", ex);
-    }
-}
+                JsonDocument doc = JsonDocument.Parse(json);
 
+                foreach (var element in doc.RootElement.EnumerateArray())
+                {
+                    string countryName = element.GetProperty("name").GetString();
+                    string currencyCode = element.GetProperty("currencies").EnumerateArray().First().GetProperty("code").GetString();
+                    string currencySymbol = element.GetProperty("currencies").EnumerateArray().First().GetProperty("symbol").GetString();
 
+                    LocalCurrency localCurrency = new LocalCurrency
+                    {
+                        CountryName = countryName,
+                        CurrencyCode = currencyCode,
+                        CurrencySymbol = currencySymbol
+                    };
 
+                    return localCurrency;
+                }
+
+                throw new JsonException("No valid currency information found in the JSON array.");
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new ApplicationException("Failed to retrieve currency info. Check your network connection.", ex);
+            }
+            catch (JsonException ex)
+            {
+                throw new JsonException("Failed to parse JSON response.", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("An error occurred while retrieving currency info.", ex);
+            }
+        }
 
 
         /// <summary>
@@ -114,19 +96,51 @@ public LocalCurrency GetLocalCurrencyByAlpha2Or3Code(string? alpha2Or3Code)
         /// <param name="token">Token for cancellation asynchronous operation.</param>
         /// <returns>Information about country currency as <see cref="LocalCurrency"/>>.</returns>
         /// <exception cref="ArgumentException">Throw if countryCode is null, empty, whitespace or invalid country code.</exception>
-        public async Task<LocalCurrency> GetLocalCurrencyByAlpha2Or3CodeAsync(string? alpha2Or3Code, CancellationToken token)
+        public async Task<LocalCurrency> GetLocalCurrencyByAlpha2Or3CodeAsync(string? alpha2Or3Code,
+            CancellationToken token)
         {
             if (string.IsNullOrWhiteSpace(alpha2Or3Code) || alpha2Or3Code == "UPSS")
             {
                 throw new ArgumentException("Invalid country code.");
             }
 
-            string apiUrl = $"{serviceUrl}/alpha/{alpha2Or3Code}";
-            string json = await httpClient.GetStringAsync(apiUrl, token);
-            
-            LocalCurrency currency = JsonSerializer.Deserialize<LocalCurrency>(json);
+            try
+            {
+                string apiUrl = $"{serviceUrl}/alpha/{Uri.EscapeDataString(alpha2Or3Code)}";
+                string json = await httpClient.GetStringAsync(apiUrl, token);
 
-            return currency;
+                JsonDocument doc = JsonDocument.Parse(json);
+
+                foreach (var element in doc.RootElement.EnumerateArray())
+                {
+                    string countryName = element.GetProperty("name").GetString();
+                    string currencyCode = element.GetProperty("currencies").EnumerateArray().First().GetProperty("code").GetString();
+                    string currencySymbol = element.GetProperty("currencies").EnumerateArray().First().GetProperty("symbol").GetString();
+
+                    LocalCurrency localCurrency = new LocalCurrency
+                    {
+                        CountryName = countryName,
+                        CurrencyCode = currencyCode,
+                        CurrencySymbol = currencySymbol
+                    };
+
+                    return localCurrency;
+                }
+
+                throw new JsonException("No valid currency information found in the JSON array.");
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new ApplicationException("Failed to retrieve currency info. Check your network connection.", ex);
+            }
+            catch (JsonException ex)
+            {
+                throw new JsonException("Failed to parse JSON response.", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("An error occurred while retrieving currency info.", ex);
+            }
         }
 
         /// <summary>
@@ -147,20 +161,16 @@ public LocalCurrency GetLocalCurrencyByAlpha2Or3Code(string? alpha2Or3Code)
                 string apiUrl = $"{serviceUrl}/capital/{Uri.EscapeDataString(capital)}";
                 string json = httpClient.GetStringAsync(apiUrl).Result;
 
-                // Parse the JSON response into an array of JSON elements.
                 JsonDocument doc = JsonDocument.Parse(json);
 
-                // Assuming the JSON response is an array, loop through the elements.
                 foreach (var element in doc.RootElement.EnumerateArray())
                 {
-                    // Extract the properties you need from each element.
                     string name = element.GetProperty("name").GetString();
                     string capitalName = element.GetProperty("capital").GetString();
                     double area = element.GetProperty("area").GetDouble();
                     int population = element.GetProperty("population").GetInt32();
                     string flag = element.GetProperty("flag").GetString();
 
-                    // Create and return a CountryInfo object.
                     CountryInfo countryInfo = new CountryInfo
                     {
                         Name = name,
@@ -208,12 +218,10 @@ public LocalCurrency GetLocalCurrencyByAlpha2Or3Code(string? alpha2Or3Code)
                 string apiUrl = $"{serviceUrl}/capital/{Uri.EscapeDataString(capital)}";
                 string json = await httpClient.GetStringAsync(apiUrl);
 
-                // Parse the JSON response into an array of JSON elements.
                 JsonDocument doc = JsonDocument.Parse(json);
 
                 foreach (var element in doc.RootElement.EnumerateArray())
                 {
-                    // Extract the properties you need from each element.
                     string name = element.GetProperty("name").GetString();
                     string capitalName = element.GetProperty("capital").GetString();
                     double area = element.GetProperty("area").GetDouble();
@@ -247,6 +255,5 @@ public LocalCurrency GetLocalCurrencyByAlpha2Or3Code(string? alpha2Or3Code)
                 throw new ApplicationException("An error occurred while retrieving country info.", ex);
             }
         }
-
     }
 }
